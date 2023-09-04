3de99c25767cc71a0ead54d6143e9383221a0df2
lucashthiele <lucasthiele0@outlook.com>
2023-09-04 20:53:39 -0300

    feat: implement RatingControllerTest get method

--- src/test/java/com/lucashthiele/milhas/controllers/RatingControllerTest.java
+++ src/test/java/com/lucashthiele/milhas/controllers/RatingControllerTest.java
@@ -15,10 +15,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -33,6 +37,8 @@ public class RatingControllerTest {
     private JacksonTester<RatingDTO> ratingDTOJSON;
     @Autowired
     private JacksonTester<Rating> ratingJSON;
+    @Autowired
+    private JacksonTester<List<Rating>> ratingListJSON;
 
     @Test
     @DisplayName("it should return status 200 when valid data was sent")
@@ -50,7 +56,7 @@ public class RatingControllerTest {
         ).andReturn().getResponse();
 
         // Validate HTTP Status Code to be 200
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat200(response.getStatus());
 
         var expectedJSON = ratingJSON.write(
                 rating
@@ -58,5 +64,30 @@ public class RatingControllerTest {
 
         // Validate JSON sent back by the api
         assertThat(response.getContentAsString()).isEqualTo(expectedJSON);
+    }
+
+    @Test
+    @DisplayName("it should return an array of Ratings")
+    void getRatings200() throws Exception {
+        var ratingDTO = new RatingDTO("User Name","Rating Text", "https://picture-ulr.com");
+        var rating = new Rating(ratingDTO);
+        var mockRatingList = new ArrayList<Rating>();
+        mockRatingList.add(rating);
+
+        when(ratingService.findAll()).thenReturn(mockRatingList);
+
+        var response = mvc.perform(get("/depoimentos")).andReturn().getResponse();
+
+        assertThat200(response.getStatus());
+
+        var expectedJSON = ratingListJSON.write(
+                mockRatingList
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(expectedJSON);
+    }
+
+    private void assertThat200(int statusCode){
+        assertThat(statusCode).isEqualTo(HttpStatus.OK.value());
     }
 }
